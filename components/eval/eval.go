d8156eb2431f6c68076d67ac07f815606b19fb25
Maksym Trofimenko <gtpoxa@gmail.com>
2025-04-21 21:02:06 +0100

    updates module

--- components/eval/eval.go
+++ components/eval/eval.go
@@ -31,7 +31,7 @@ type OutputData any
 
 type Script struct {
 	Name    string `json:"name" required:"true" title:"File name" Description:"e.g. utils.js"`
-	Content string `json:"content" required:"true" title:"Javascript code" format:"code"`
+	Content string `json:"content" required:"true" language:"javascript" title:"Javascript code" format:"code"`
 }
 
 // ScriptItem to avoid confusion of Script definition generated from Scripts array and from Script property
