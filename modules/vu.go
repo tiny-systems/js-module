f4c0cd8b8b0a4d4bca1432561efc6aae95883f6c
Maksym Trofimenko <gtpoxa@gmail.com>
2025-04-21 16:30:21 +0100

    initial commit

---
+++ modules/vu.go
@@ -0,0 +1,23 @@
+package modules
+
+import (
+	"context"
+	"github.com/grafana/sobek"
+)
+
+type ModuleVU struct {
+	ctx     context.Context
+	runtime *sobek.Runtime
+}
+
+func NewModuleVU(ctx context.Context, runtime *sobek.Runtime) *ModuleVU {
+	return &ModuleVU{ctx: ctx, runtime: runtime}
+}
+
+func (m *ModuleVU) Context() context.Context {
+	return m.ctx
+}
+
+func (m *ModuleVU) Runtime() *sobek.Runtime {
+	return m.runtime
+}
